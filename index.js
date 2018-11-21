6bb609a40964f5e49bf6b1d19027d8a956685138
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-21 21:01:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- 
 
-client.on('ready', () => {
-
+ client.on('ready', () => {
     console.log('I am ready!');
-
 });
 
- 
+if(cmd === `${prefix}serverinfo`){
 
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
+        let serverembed = new Discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#009dff")
+        .addField("Server Name", message.guild.name)
+        .addField("Total Members", message.guild.memberCount);
+    
+        return message.channel.send(serverembed);
+    }
 
 });
 
