93a275d0a07021b61a351762e4f2448e5fd397b6
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-21 21:46:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const botconfig = require("./botconfig.json");
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
-const bot = new Discord.Client({disableEveryone: true});
-const prefix = "!";
+const client = new Discord.Client();
 
-bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online!`);
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
 client.on('message', message => {
@@ -16,30 +18,7 @@ client.on('message', message => {
 
        }
 
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type ==="dm") return;
-
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if(cmd === `${prefix}hello`){
-        return message.channel.send("Hello!")
-    }
-
-    if(cmd === `${prefix}serverinfo`){
-
-        let serverembed = new Discord.RichEmbed()
-        .setDescription("Server Information")
-        .setColor("#009dff")
-        .addField("Server Name", message.guild.name)
-        .addField("Total Members", message.guild.memberCount);
-    
-        return message.channel.send(serverembed);
-    }
-
 });
 
-bot.login(process.env.BOT_TOKEN);
+
+client.login(process.env.BOT_TOKEN);
