e5b731350ca0f89ab738826eb7078c37a60033ab
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-21 21:31:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-const client = new Discord.Client();
+const client = new Discord.Client({disableEveryone: true});
 const prefix = "!";
  
-client.on('ready', () => {
+client.on("ready", async () => {
     console.log('I am ready!');
 });
 
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type ==="dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
  
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-
-    }
-if(cmd === `${prefix}hello`){
-        return message.channel.send("Hello!")
-    }
+ 
  
 });
 
