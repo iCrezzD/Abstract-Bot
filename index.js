dffdec43bc0179e8c7ea3da419d3083004c5b6b5
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-21 20:50:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Discord = require('discord.js');
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
 
- 
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
@@ -33,5 +33,6 @@ bot.on("message", async message => {
     }
 
 });
+
 
 client.login(process.env.BOT_TOKEN);
