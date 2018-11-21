a8cff24c972c8cd6745ab484d460780b8585d632
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-21 21:56:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,30 +10,14 @@ client.on('ready', () => {
 
 });
 
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type ==="dm") return;
+client.on('message', message => {
 
-    let prefix = "!";
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
+    if (message.content === 'ping') {
 
-if(cmd === `${prefix}hello`){
-        return message.channel.send("Hello!")
-}
+       message.reply('pong');
 
-if(cmd === `${prefix}serverinfo`){
+       }
 
- let serverembed = new Discord.RichEmbed()
-        .setDescription("Server Information")
-        .setColor("#009dff")
-        .addField("Server Name", message.guild.name)
-        .addField("Total Members", message.guild.memberCount);
-    
-        return message.channel.send(serverembed);
-}
- 
 });
 
 
