a6802b501d460a45e73613dc264a281c853bc772
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-21 20:17:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`);
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type ==="dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hello`){
+        return message.channel.send("Hello!")
+    }
+
+    if(cmd === `${prefix}serverinfo`){
+
+        let serverembed = new Discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#009dff")
+        .addField("Server Name", message.guild.name)
+        .addField("Total Members", message.guild.memberCount);
+    
+        return message.channel.send(serverembed);
+    }
+
+});
+
+bot.login(botconfig.token);
