cf78209b93e4f59129a19bc9664f79242c612ec5
iCrezzD <32820729+iCrezzD@users.noreply.github.com>
2018-11-24 22:55:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,47 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
+ 
+ 
     if (message.content === 'ping') {
 
        message.reply('pong');
 
        }
 
+ 
+ 
+   if(message.content === `!serverinfo`){
+
+    function checkBots(guild) {
+      let botCount = 0; 
+      guild.members.forEach(member => { 
+        if(member.user.bot) botCount++; 
+      });
+      return botCount; 
+    }
+  
+    function checkMembers(guild) {
+      let memberCount = 0;
+      guild.members.forEach(member => {
+        if(!member.user.bot) memberCount++; 
+      });
+      return memberCount;
+    }
+
+    let serverembed = new Discord.RichEmbed()
+    .setColor("#99AAB5")
+    .addField("You Joined", message.member.joinedAt)
+    .addField('Humans', checkMembers(message.guild), true)
+    .addField('Bots', checkBots(message.guild), true)
+    .addField("Total Members", message.guild.memberCount)
+    .setFooter('Server created at:')
+    .setTimestamp(message.guild.createdAt);
+
+    return message.channel.send(serverembed);
+  }
+
+ 
 });
 
 
