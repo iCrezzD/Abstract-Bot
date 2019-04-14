ffdde9c7fb8a1fe4b46069b272df4f1149e13dba
iamabstractt <32820729+iamabstractt@users.noreply.github.com>
2019-04-14 13:19:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,36 +56,6 @@ client.on('message', message => {
   
   
   
-  if(message.content === '!report'){
-
-      //!report @abstract this is the reason
-
-      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("Couldn't find user.");
-      let rreason = args.join(" ").slice(22);
-
-      let reportEmbed = new Discord.RichEmbed()
-      .setDescription("Reports")
-      .setColor("#99AAB5")
-      .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-      .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-      .addField("Channel", message.channel)
-      .addField("Time", message.createdAt)
-      .addField("Reason", reason);
-
-      let reportschannel = message.guild.channels.find(`name`, "reports");
-      if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-
-
-      message.delete().catch(O_o=>{});
-      reportschannel.send(reportEmbed);
-
-      return;
-    }
-  
-  
-  
-  
     if(message.content === '!help'){
 
     let serverembed = new Discord.RichEmbed()
