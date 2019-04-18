0205f815d5bde09c3d82679f008722060c5bf98a
iamabstractt <32820729+iamabstractt@users.noreply.github.com>
2019-04-18 22:40:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
 
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
@@ -19,81 +37,9 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  
-  if(cmd === `${prefix}ping`) {
-      message.reply(' the ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-    }
-
-
-  if(cmd === `${prefix}serverinfo`){
-
-      function checkBots(guild) {
-        let botCount = 0;
-        guild.members.forEach(member => {
-          if(member.user.bot) botCount++;
-        });
-        return botCount;
-      }
-
-      function checkMembers(guild) {
-        let memberCount = 0;
-        guild.members.forEach(member => {
-          if(!member.user.bot) memberCount++;
-        });
-        return memberCount;
-      }
-
-      let serverembed = new Discord.RichEmbed()
-      .setColor("#99AAB5")
-      .addField("You Joined", message.member.joinedAt)
-      .addField("Humans", checkMembers(message.guild), true)
-      .addField("Bots", checkBots(message.guild), true)
-      .addField("Total Members", message.guild.memberCount)
-      .setFooter("Server created at:")
-      .setTimestamp(message.guild.createdAt);
-
-      return message.channel.send(serverembed);
-    }
-  
-  if(cmd === `${prefix}report`){
-
-      //!report @abstract this is the reason
-
-      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!rUser) return message.channel.send("Couldn't find user.");
-      let rreason = args.join(" ").slice(22);
-
-      let reportEmbed = new Discord.RichEmbed()
-      .setDescription("Reports")
-      .setColor("#99AAB5")
-      .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-      .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-      .addField("Channel", message.channel)
-      .addField("Time", message.createdAt)
-      .addField("Reason", rreason);
-
-      let reportschannel = message.guild.channels.find(`name`, "reports");
-      if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
-
-
-      message.delete().catch(O_o=>{});
-      reportschannel.send(reportEmbed);
-
-      return;
-    }
-  
-    if(cmd === `${prefix}!help`){
-
-    let serverembed = new Discord.RichEmbed()
-    .setColor("#99AAB5")
-    .addField("!ping", "Checks the bot's response time to Discord.", true)
-    .addBlankField(true)
-    .addField("!serverinfo", "Shows all important info about your server.", true);
-
-    return message.channel.send(serverembed);
-  }
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 
 });
 
 bot.login(process.env.BOT_TOKEN);
-
