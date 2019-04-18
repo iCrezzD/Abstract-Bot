f8eab2941da8b6c1009e621a1a3fa1f439ee8981
iamabstractt <32820729+iamabstractt@users.noreply.github.com>
2019-04-18 15:24:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,88 @@
-const Discord = require('discord.js');
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
 
-const client = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 
 
-
-client.on('ready', () => {
-  console.log('I am ready!');
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
   
     client.user.setActivity(` ${client.users.size} users`, { type: 'WATCHING' });
 });
 
-client.on('message', message => {
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
   
-  
-
-  if (message.content === '!ping') {
+  if(cmd === `${prefix}ping`) {
       message.reply(' the ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
-
-  }
-
-
-
-  
-  if(message.content === '!serverinfo'){
-
-    function checkBots(guild) {
-      let botCount = 0; 
-      guild.members.forEach(member => { 
-        if(member.user.bot) botCount++; 
-      });
-      return botCount; 
-    }
-  
-    function checkMembers(guild) {
-      let memberCount = 0;
-      guild.members.forEach(member => {
-        if(!member.user.bot) memberCount++; 
-      });
-      return memberCount;
     }
 
-    let serverembed = new Discord.RichEmbed()
-    .setColor("#99AAB5")
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Humans", checkMembers(message.guild), true)
-    .addField("Bots", checkBots(message.guild), true)
-    .addField("Total Members", message.guild.memberCount)
-    .setFooter("Server created at:")
-    .setTimestamp(message.guild.createdAt);
 
-    return message.channel.send(serverembed);
-  }
+  if(cmd === `${prefix}serverinfo`){
 
+      function checkBots(guild) {
+        let botCount = 0;
+        guild.members.forEach(member => {
+          if(member.user.bot) botCount++;
+        });
+        return botCount;
+      }
+
+      function checkMembers(guild) {
+        let memberCount = 0;
+        guild.members.forEach(member => {
+          if(!member.user.bot) memberCount++;
+        });
+        return memberCount;
+      }
+
+      let serverembed = new Discord.RichEmbed()
+      .setColor("#99AAB5")
+      .addField("You Joined", message.member.joinedAt)
+      .addField("Humans", checkMembers(message.guild), true)
+      .addField("Bots", checkBots(message.guild), true)
+      .addField("Total Members", message.guild.memberCount)
+      .setFooter("Server created at:")
+      .setTimestamp(message.guild.createdAt);
+
+      return message.channel.send(serverembed);
+    }
   
+  if(cmd === `${prefix}report`){
+
+      //!report @abstract this is the reason
+
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+      if(!rUser) return message.channel.send("Couldn't find user.");
+      let rreason = args.join(" ").slice(22);
+
+      let reportEmbed = new Discord.RichEmbed()
+      .setDescription("Reports")
+      .setColor("#99AAB5")
+      .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+      .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+      .addField("Channel", message.channel)
+      .addField("Time", message.createdAt)
+      .addField("Reason", rreason);
+
+      let reportschannel = message.guild.channels.find(`name`, "reports");
+      if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
+
+
+      message.delete().catch(O_o=>{});
+      reportschannel.send(reportEmbed);
+
+      return;
+    }
   
-  
-    if(message.content === '!help'){
+    if(cmd === `${prefix}!help`){
 
     let serverembed = new Discord.RichEmbed()
     .setColor("#99AAB5")
@@ -67,13 +93,7 @@ client.on('message', message => {
     return message.channel.send(serverembed);
   }
 
-  
-  
-  
-  
-  
 });
 
-
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
 
