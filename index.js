800e84bf23e49cb6b27baa9bb55dd0bbeef7844d
iamabstractt <32820729+iamabstractt@users.noreply.github.com>
2019-04-18 15:41:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   
-    client.user.setActivity(` ${client.users.size} users`, { type: 'WATCHING' });
+    bot.user.setActivity(` ${client.users.size} users`, { type: 'WATCHING' });
 });
 
 bot.on("message", async message => {
