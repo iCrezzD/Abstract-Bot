3f4c69546ba5bb73c3a6ac655004b99592e12a71
iamabstractt <32820729+iamabstractt@users.noreply.github.com>
2019-05-18 22:44:08 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.roles.some(r=>["Administrators", "Moderators", "Trial Mods"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Couldn't find user.");
     let rreason = args.join(" ").slice(22);
