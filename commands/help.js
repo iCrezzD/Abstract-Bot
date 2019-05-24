d7bcaeb39e3216c30ef8eb8f0424902a152a43a7
iamabstractt <32820729+iamabstractt@users.noreply.github.com>
2019-05-24 23:56:24 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,9 +7,11 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#99AAB5")
   .addField("!help", "Shows info about commands.")
   .addField("!ping", "Checks the bot's response time to Discord.")
-  .addField("!serverinfo", "Shows info about the server")
+  .addField("!serverinfo", "Shows info about the server.")
   .addField("!report (user) (reason)", "Reports user to reports channel.")
-  .addField("!clear (number)", "Clears 2-100 messages from channel.");
+  .addField("!clear (number)", "Clears 2-100 messages from channel.")
+  .addField("!kick (user) (reason)", "Kicks a member from the server.")
+  .addField("!ban (user) (reason)", "Bans a member from the server.");
 
   message.channel.send(helpembed);
 }
